4405f6dbe3ff012f9763aa921ae19a45edf4c9b5
Christopher Wattaul <chris.wattaul@gmail.com>
2020-08-19 12:44:33 +0200

    Remove unused dependencies

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var randomColor = require('randomcolor');
+const randomColor = require('randomcolor');
 const hue = process.argv[2];
 const luminosity = process.argv[3];
-var color = randomColor({ hue: hue, luminosity: luminosity });
+const color = randomColor({ hue: hue, luminosity: luminosity });
 const guessedColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
 const chalk = require('chalk');
 
-let result = hue
+const result = hue
   ? chalk.hex(color)(
       `      ###########################
       ###########################
