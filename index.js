0c4e507d402a28f041a746d024f686635961265c
Christopher Wattaul <chris.wattaul@gmail.com>
2020-08-19 12:53:25 +0200

    Add .replit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const randomColor = require('randomcolor');
+const chalk = require('chalk');
 const hue = process.argv[2];
 const luminosity = process.argv[3];
 const color = randomColor({ hue: hue, luminosity: luminosity });
 const guessedColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-const chalk = require('chalk');
 
 const result = hue
   ? chalk.hex(color)(
